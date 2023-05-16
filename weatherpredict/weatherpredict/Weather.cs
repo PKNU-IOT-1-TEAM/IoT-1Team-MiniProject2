c3ae93627027e2ddc17a134ba2f27bdbd6be80c1
drowned <fight1999club@gmail.com>
2023-05-16 17:26:58 +0900

    .

---
+++ weatherpredict/weatherpredict/Weather.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weatherpredict
+{
+    public class Weather
+    {
+        public string FcstDate { get; set; }
+        public string FcstTime { get; set; }
+        public string T1H { get; set; }
+        public string RN1 { get; set; }
+        public string SKY { get; set; }
+        public string UUU { get; set; }
+        public string VVV { get; set; }
+        public string REH { get; set; }
+        public string PTY { get; set; }
+        public string LGT { get; set; }
+        public string VEC { get; set; }
+        public string WSD { get; set; }
+    }
+}
