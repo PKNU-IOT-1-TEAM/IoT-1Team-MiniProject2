3c880529d57ececd1152f26d9b2dab37a01870ba
JuHyunLee99 <juhyun.lee0829@daum.net>
2023-07-21 12:11:23 +0900

    클라우드 비번 변경

--- Nobless-CHIM-SOY-Monitoring/Nobless-CHIM-SOY-Monitoring/Logics/Commons.cs
+++ Nobless-CHIM-SOY-Monitoring/Nobless-CHIM-SOY-Monitoring/Logics/Commons.cs
@@ -15,7 +15,7 @@ namespace Nobless_CHIM_SOY_Monitoring.Logics
                                                      "Port=3306;" +
                                                      "Database=miniproject02;" +
                                                      "Uid=pknuiot1team;" +
-                                                     "Pwd=2V3lhihd8gIQ3krjNMf2;";
+                                                     "Pwd=oRaCcM7JUOQ2zzhxTswT;";
 
         // CCTV용 : api받은 영상은 60초만 제공 => 해결방법으로 고유주소 찾아서 사용
         public static readonly string CCTV_Url = "http://61.43.246.226:1935/rtplive/cctv_193.stream/playlist.m3u8";
